b903ec9701821521c3b02ac99b318e2d7164b7ed
Pedro Martinez <pedrocmartinez568@gmail.com>
2023-07-30 22:14:43 -0600

    adding new component and react

---
+++ src/ReactComponents/Example.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+export default function Example(){
+  const [counter, setCounter] = useState(0)
+  return (
+    <div className="w-full flex flex-col gap-3 min-h-[300px] justify-center items-center text-xl " >
+      <h3>React component</h3> 
+      <div className='flex gap-3 items-center ' >
+        <button 
+          className='px-3 py-1 bg-gray-400 rounded hover:bg-gray-300 '
+          onClick={() => {
+            setCounter(counter - 1)
+          }}
+        >-</button>
+        <div>{counter}</div>
+        <button 
+          className='px-3 py-1 bg-gray-400 hover:bg-gray-300 rounded'
+          onClick={() => {
+            setCounter(counter + 1)
+          }} 
+        >+
+        </button>
+      </div>
+
+    </div>
+  )
+}
