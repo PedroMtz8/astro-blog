5bb862aebeec0a4aa9d95da32756fe57ea1d37a2
Pedro Martinez <pedrocmartinez568@gmail.com>
2023-08-05 23:21:04 -0600

    remove github config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,4 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://pedromtz8.github.io',
-  base: '/astro-blog'
 });
