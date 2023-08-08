ccf3845fcc4f90c03706dd698ed75124cd49084a
Pedro Martinez <pedrocmartinez568@gmail.com>
2023-08-08 00:02:06 -0600

    changed typed animation package

--- src/ReactComponents/Typed.tsx
+++ src/ReactComponents/Typed.tsx
@@ -1,13 +1,13 @@
-import Typed from 'react-typed';
+// import Typed from 'react-typed';
 
-export default function TypedText({ text }: { text: string }) {
-  return(
-    <Typed
-      strings={[text]}
-      typeSpeed={50}
-      backSpeed={70}
-      backDelay={1500}
-      loop
-    />
-  )
-}
+// export default function TypedText({ text }: { text: string }) {
+//   return(
+//     <Typed
+//       strings={[text]}
+//       typeSpeed={50}
+//       backSpeed={70}
+//       backDelay={1500}
+//       loop
+//     />
+//   )
+// }
---
+++ src/ReactComponents/Typping.tsx
@@ -0,0 +1,7 @@
+import { Typewriter } from 'react-simple-typewriter'
+
+export default function TypeAnimationText({ text }: { text: string }) {
+  return(
+      <Typewriter words={[text]} typeSpeed={50} delaySpeed={1500} deleteSpeed={80} loop />
+  )
+}
