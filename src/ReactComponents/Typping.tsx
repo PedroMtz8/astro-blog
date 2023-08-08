4faa6c0fc9992d28dadeefbe838d63ab65caf2eb
Pedro Martinez <pedrocmartinez568@gmail.com>
2023-08-08 00:09:19 -0600

    change color and adding cursor to typed animation text

--- src/ReactComponents/Typping.tsx
+++ src/ReactComponents/Typping.tsx
@@ -1,7 +1,10 @@
-import { Typewriter } from 'react-simple-typewriter'
+import { Typewriter, Cursor  } from 'react-simple-typewriter'
 
 export default function TypeAnimationText({ text }: { text: string }) {
   return(
+    <>
       <Typewriter words={[text]} typeSpeed={50} delaySpeed={1500} deleteSpeed={80} loop />
+      <Cursor cursorColor='#4FD1C5' />
+    </>
   )
 }
