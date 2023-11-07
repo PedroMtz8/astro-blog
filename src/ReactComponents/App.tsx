81cb89c7e3d38a7d5db26f2c1f444d8afe270e14
Pedro Martinez <104749449+PedroMtz8@users.noreply.github.com>
2023-11-06 22:33:36 -0600

    checking if works

--- src/ReactComponents/App.tsx
+++ src/ReactComponents/App.tsx
@@ -1,8 +1,17 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import Example from './Example'
 import MyModal from './Modal'
+import { useState, useEffect } from 'react'
 
 export default function App(){
+  const [mounted, setMounted] = useState(false);
+  console.log('mounted', mounted);
+  useEffect(() => {
+    setMounted(true);
+  }, [])
+
+  if (!mounted) return null
+
   return (
     <ChakraProvider>
       <Example />
