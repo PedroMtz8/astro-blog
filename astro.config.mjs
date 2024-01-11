79086bb7a06b06cb4f12fecaf0adc079f7e88a1e
Pedro Martinez <pedrocmartinez568@gmail.com>
2024-01-10 23:42:43 -0600

    auth config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,27 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
+import { loadEnv } from 'vite';
+import auth from 'auth-astro'
+import Google from "@auth/core/providers/google"
+
+const env = loadEnv('production', process.cwd(), '');
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), 
+    auth({
+      providers: [
+        Google({
+          clientId: env.GOOGLE_ID,
+          clientSecret: env.GOOGLE_SECRET
+        })
+      ]
+    })
+  ],
   output: 'server',
-  adapter: vercel()
+  adapter: vercel({
+    analytics: true,
+  })
 });
