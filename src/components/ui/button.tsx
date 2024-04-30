7f39cd3be2f2a4abab7323541f28e37595214f85
Pedro Martinez <pedrocmartinez568@gmail.com>
2024-04-29 18:06:14 -0700

    add Intersection Observer

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,11 @@
+
+export default function Button({ children, ...props }) {
+  return (
+    <button 
+      {...props} 
+      className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+    >
+      {children}
+    </button>
+  )
+  }
