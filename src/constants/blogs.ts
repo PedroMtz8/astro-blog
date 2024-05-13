ec6dfc6ced0b3034d8fa531931af06183c92a639
Pedro Martinez <pedrocmartinez.dev@gmail.com>
2024-05-12 21:48:30 -0600

    add intersection observer

---
+++ src/constants/blogs.ts
@@ -0,0 +1,23 @@
+export const blogs = [
+  {
+    id: 1,
+    title: 'Blog 1',
+    description: 'Descripción del blog 1',
+    image: 'https://via.assets.so/game.png?id=1&q=40',
+    link: '/blog/1'
+  },
+  {
+    id: 2,
+    title: 'Blog 2',
+    description: 'Descripción del blog 2',
+    image: 'https://via.assets.so/game.png?id=2&q=40',
+    link: '/blog/2'
+  },
+  {
+    id: 3,
+    title: 'Blog 3',
+    description: 'Descripción del blog 3',
+    image: 'https://via.assets.so/game.png?id=3&q=40',
+    link: '/blog/3'
+  }
+];
---
+++ src/store/pages/index.ts
@@ -0,0 +1,4 @@
+import { atom } from 'nanostores';
+
+export const thresholdState = atom(1);
+export const isOpen = atom(false);
